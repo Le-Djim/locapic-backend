263517d2ab17adee01ce3f05db15fc968a8fefc0
Jimmy Bellavia <bellavia.jimmy@gmail.com>
2023-10-15 15:11:32 +0200

    last commit

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 require('../models/connection');
-const Place = require('../models/places')
+const Place = require('../models/places');
+const { checkBody } = require('../modules/checkBody');
 
 router.post('/places', (req, res) => {
-    const { nickname, name, latitude, longitude } = req.body;
-    const newPlace = new Place({ nickname, name, latitude, longitude });
-   
-    newPlace.save().then(() => {
-      res.json({ result: true });
-    });
-   });
+  if (!checkBody(req.body, ['nickname', 'name', 'latitude', 'longitude'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  const { nickname, name, latitude, longitude } = req.body;
+  const newPlace = new Place({ nickname, name, latitude, longitude });
+
+  newPlace.save().then(() => {
+    res.json({ result: true });
+  });
+});
 
 router.get('/places/:nickname', (req, res) => {
-    // Récupération du surnom depuis les paramètres de l'URL
-    const nickname = req.params.nickname;
-
-    // Recherche dans la base de données
-    Place.find({ nickname: nickname })
-    .then(places => {
-    // Renvoyez les lieux trouvés
-        res.json({ result: true, places: places });
-    });
+  // Regex to find places regardless of nickname case
+  Place.find({ nickname: { $regex: new RegExp(req.params.nickname, 'i') } }).then(data => {
+      res.json({ result: true, places: data });
+  });
 });
 
 router.delete('/places', (req, res) => {
-    const { nickname, name } = req.body;
+  if (!checkBody(req.body, ['nickname', 'name'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
 
-    Place.findOneAndDelete({ nickname: nickname, name: name })
-        .then((deletedPlace) => {
-            if (deletedPlace) {
-                res.json({ result: true });
-            } else {
-                // Aucun lieu trouvé avec le surnom et le nom donnés
-                res.json({ result: false, message: "No place found with given nickname and name." });
-            }
-        })
-    });
+  const { nickname, name } = req.body;
+
+  // Regex to delete place regardless of nickname case
+  Place.deleteOne({ nickname: { $regex: new RegExp(nickname, 'i') }, name }).then((deletedDoc) => {
+    if (deletedDoc.deletedCount > 0) {
+      res.json({ result: true });
+    } else {
+      res.json({ result: false, error: 'Place not found' });
+    }
+  });
+});
 
 module.exports = router;
